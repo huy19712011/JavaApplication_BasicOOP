bbae1ceea47ce6ad5a4bd202118c64319ea5443b
huy19712011 <huy19712011@gmail.com>
2021-04-25 09:51:40 +0700

    end of 6: Adding EmailApp, Email; adding fields for Email

---
+++ src/emailapp/Email.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package emailapp;
+
+/**
+ *
+ * @author huynq
+ */
+public class Email {
+    
+    private String firstName;
+    private String lastName;
+    private String password;
+    private String department;
+    private int mailboxCapacity;
+    private String alternateEmail;
+    
+    // Constructor for firstName, lastName
+    
+    // Ask for the department
+    
+    // Generate a random password
+    
+    // Set the mailbox capacity
+    
+    // Set the alternate email
+    
+    // Change the password
+    
+    
+            
+    
+}
